be3d6d2010ab75ebc47b1412ccf2e253d4848398
Keanu73 <guestone63@gmail.com>
2019-03-13 17:54:09 +0000

    Speedy statistics

---
+++ commands/whovoted.js
@@ -0,0 +1,16 @@
+exports.run = async (client, cmd, args) => {
+    if (!(cmd.player.name === client.president)) return
+    let cands = ""
+    
+    for (const c in client.citizens) {
+      if (!client.votes.get(client.citizens[c]).voted) return
+      cands += `\n§a§l${client.citizens[c]}§r\n`
+    }
+    
+    await cmd.reply(`\n======== §6§l§nList of Citizens who Voted:§r ========\n${cands}\n==========================================\n`, client.label, "format")
+}
+  
+  exports.conf = {
+    name: "votecount",
+    permLevel: "1"
+  }
