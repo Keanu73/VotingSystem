82dfbf0f567cf81f349c8ef2eddc1cc755e538f6
Keanu73 <guestone63@gmail.com>
2019-03-13 18:42:32 +0000

    This might work..?

--- commands/eval.js
+++ commands/eval.js
@@ -1,8 +1,8 @@
 exports.run = async (client, cmd, args) => {
     if (!(cmd.player.name === client.president)) return
     try {
-        const code = args[0]
-        const result = eval(code.toString())
+        const code = args.join(" ")
+        const result = eval(code)
         console.log(code)
         console.log(result)
     } catch (err) {
